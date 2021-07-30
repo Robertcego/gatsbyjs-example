c079b60dc9678c8cfc4b619365bcf5c576ed3044
Robertcego <lrobertito@gmail.com>
2021-07-30 14:44:57 -0600

    who needs react-router? :D

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import * as React from "react";
-import { useState } from "react";
+import { Link } from 'gatsby';
+import * as React from 'react';
+import { useState } from 'react';
 
 // markup
 const IndexPage = () => {
@@ -9,13 +10,15 @@ const IndexPage = () => {
     <div>
       <h1>Hello World!</h1>
       <button
-        type="button"
+        type='button'
         onClick={() => (counter > 0 ? setCounter(counter - 1) : counter)}
       >
         Decrement
       </button>
       <h2>Count: {counter}</h2>
       <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <h2>Go to About page</h2>
+      <Link to='/about'>About</Link>
     </div>
   );
 };
